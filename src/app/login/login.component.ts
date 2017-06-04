9d86b554c53040819a4715b1e745c9ce0ac4ea2f
puricamaykol <puricamaykol@gmail.com>
2017-06-04 13:24:15 -0400

    nuevo build

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,7 +11,6 @@ export class LoginComponent  {
   _baseUrl: string = 'https://accounts.google.com/o/oauth2/v2/auth';
   _parameters: {} = {
 	  "client_id": "35272062455-m15eim6cnehf2v6bajd44a5o289jtg1h.apps.googleusercontent.com",
-	  //"redirect_uri": "http://localhost/angularMpListTest/dist/logincb",
 	  "redirect_uri": "https://puricamaykol.github.io/angularLogin/dist/logincb",
 	  "response_type": "token",
 	  "scope": "https://www.googleapis.com/auth/user.emails.read https://www.googleapis.com/auth/user.phonenumbers.read https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
